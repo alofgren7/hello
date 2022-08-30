3fc144ea72f5376d49b7ac64482a726f53716e05
Andrew Lofgren <alofgren7@huskers.unl.edu>
2022-08-30 01:53:14 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Andrew Lofgren
+ *         alofgren7@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Andrew Lofgren!\n");
+  printf("Major: Computer Science\n");
+
+  return 0;
+}
